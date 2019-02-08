bacdd7e3372a5d8ac24be6944dbdcee97f33c9dd
Elizabeth Rogers <beth3346@gmail.com>
2019-02-07 23:06:28 -0600

    remove babel stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,20 @@
-import gulp from 'gulp';
-import path from 'path';
-import sass from 'gulp-sass';
-import scsslint from 'gulp-scss-lint';
-import plumber from 'gulp-plumber';
-import autoprefixer from 'gulp-autoprefixer';
-import cleanCSS from 'gulp-clean-css';
-import babel from 'gulp-babel';
+const gulp = require('gulp');
+const path = require('path');
+const sass = require('gulp-sass');
+const scsslint = require('gulp-scss-lint');
+const plumber = require('gulp-plumber');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
 var paths = {
   app: 'dist/',
   css: 'dist/css/'
 };
 
-gulp.task('scsslint', () => {
-  return gulp.src('src/**/*.scss').pipe(scsslint());
-});
-
-gulp.task('styles', ['scsslint'], () => {
+gulp.task('default', () => {
   return gulp
     .src('./*.scss')
+    .pipe(scsslint())
     .pipe(plumber())
     .pipe(
       sass({
@@ -36,5 +32,3 @@ gulp.task('styles', ['scsslint'], () => {
     .pipe(cleanCSS({ debug: true }))
     .pipe(gulp.dest(paths.css));
 });
-
-gulp.task('default', ['styles']);
